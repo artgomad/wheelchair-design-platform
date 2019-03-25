fd056cbe363ec01b4887457c27af4c9580c6e347
Pablo Ferreras <artgomad@gmail.com>
2019-03-25 13:20:01 +0100

    Collect and label update

--- wheelchair3/Algorythim to detect posture/1_collect_and_label.py
+++ wheelchair3/Algorythim to detect posture/1_collect_and_label.py
@@ -17,8 +17,11 @@ THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
 # Sitting classes
-CLASSES = ["Not Sitting", "Proper Sitting", "Leaning Forward",
-           "Leaning Backward", "Leaning Left", "Leaning Right"]
+
+# CLASSES = ["Not Sitting", "Proper Sitting", "Leaning Forward",
+#           "Leaning Backward", "Leaning Left", "Leaning Right"]
+
+CLASSES = ["Not Sitting", "Proper Sitting"]
 
 LABEL_PROP_NAME = "dhaval"
 DATA_PROP_NAME = "fsr"
