f692665f5ada8e3226d9faf9b331932faec82be0
Pablo Ferreras <artgomad@gmail.com>
2019-04-01 14:21:31 +0200

    make the try bigger

--- wheelchair3/Algorythim to detect posture/1_collect_and_label.py
+++ wheelchair3/Algorythim to detect posture/1_collect_and_label.py
@@ -84,17 +84,19 @@ def serial_to_property_values(class_index, ser):
         # Convert the bytes into string
         try:
             line = line_bytes.decode('utf-8')
+
+            # Split the string using commas as separator, we get a list of strings
+            str_values = line.split(',B')[0].split(',')
+
+            # Remove the first id
+            #str_values.pop(0)
+            # Transform the array of string values into float values (numbers)
+            values = [float(x) for x in str_values]
+
+            print(str(values))
+
         except UnicodeDecodeError:
             line = "0,0,0,0,0,0,0,0,0"
-        # Split the string using commas as separator, we get a list of strings
-        str_values = line.split(',B')[0].split(',')
-
-        # Remove the first id
-        #str_values.pop(0)
-        # Transform the array of string values into float values (numbers)
-        values = [float(x) for x in str_values]
-
-        print(str(values))
 
         # get the current time in milliseconds
         current_ts_ms = int(round(time.time() * 1000))
