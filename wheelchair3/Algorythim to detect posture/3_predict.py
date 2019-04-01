4ea2d7342aa8d4ef44ad562b621fd52a6df05f19
Pablo Ferreras <artgomad@gmail.com>
2019-04-01 15:43:14 +0200

    ,B spliting

--- wheelchair3/Algorythim to detect posture/3_predict.py
+++ wheelchair3/Algorythim to detect posture/3_predict.py
@@ -32,15 +32,21 @@ def predict(values):
 def serial_to_property_values():
     line_bytes = ser.readline()
     # If the line is not empty
-    if len(line_bytes) > 0:
+    if len(line_bytes) > 20:
         # Convert the bytes into string
-        line = line_bytes.decode('utf-8')
-        str_values = line.split(',')
-        if len(str_values) > 1:
-            str_values.pop(0)
-            values = [float(x) for x in str_values]
-            values = [values]
-            np.array(values).reshape(1, -1)
-            predict(values)
+        try:
+            line = line_bytes.decode('utf-8')
+        except:
+            line = "0,0,0,0,0,0,0,0,0"
+
+        # Split the string using commas as separator, we get a list of strings
+        str_values = line.split(',B')[0].split(',')
+        print(str_values)
+
+        values = [float(x) for x in str_values]
+        
+        values = [values]
+        np.array(values).reshape(1, -1)
+        predict(values)
 
 serial_to_property_values()
