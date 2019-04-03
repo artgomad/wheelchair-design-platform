7714d8e3631727fdf38ed0bbe9f5d520328ecda1
Pablo Ferreras <artgomad@gmail.com>
2019-04-03 14:56:43 +0200

    hello

--- wheelchair3/Algorythim to detect posture/4_predict_&_Actuate.py
+++ wheelchair3/Algorythim to detect posture/4_predict_&_Actuate.py
@@ -85,7 +85,7 @@ def predict(values, prevResult=0, counter=0):
     result = neigh.predict(values)
     print(classes[result[0]])
 
-    sendByBluetooth(result+1)
+    # sendByBluetooth(result+1)
 
     """
     if result == prevResult and result != 0:
