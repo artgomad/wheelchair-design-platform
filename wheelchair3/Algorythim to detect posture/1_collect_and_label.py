5e22e5c3f0f2db8242d8c77c583449b3d21e6403
Pablo Ferreras <artgomad@gmail.com>
2019-04-03 11:13:52 +0200

    Starting from posture 3

--- wheelchair3/Algorythim to detect posture/1_collect_and_label.py
+++ wheelchair3/Algorythim to detect posture/1_collect_and_label.py
@@ -82,7 +82,7 @@ def serial_to_property_values(class_index, ser):
     print(line_bytes)
     print(len(line_bytes))
     # If the line is not incomplete (the supose amount of bits needed is 23)
-    if len(line_bytes) > 22:
+    if len(line_bytes) > 34:
         # Convert the bytes into string
         try:
             line = line_bytes.decode('utf-8')
@@ -128,4 +128,4 @@ prop_data = my_thing.find_or_create_property(DATA_PROP_NAME, PropertyType.NINE_D
 
 
 # Start collecting data for the first class
-collect(0)
+collect(3)
