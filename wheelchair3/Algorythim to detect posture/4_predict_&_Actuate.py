67146a2e3156d1614bd436e7fe46583b07fb6cd2
Pablo Ferreras <artgomad@gmail.com>
2019-04-03 15:44:00 +0200

    f

--- wheelchair3/Algorythim to detect posture/4_predict_&_Actuate.py
+++ wheelchair3/Algorythim to detect posture/4_predict_&_Actuate.py
@@ -63,7 +63,7 @@ def discover_characteristic(device):
 
 def sendByBluetooth(x):
 
-    my_device.char_write(GATT_CHARACTERISTIC_POSTURE, x)
+    my_device.char_write(GATT_CHARACTERISTIC_POSTURE, 120)
     # print("sending: " + bytes(x).decode('utf-8'))
     print("sending: ")
     print(x)
