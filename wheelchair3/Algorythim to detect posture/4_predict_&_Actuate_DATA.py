a3aa5eaaccdf53c9f48c4d6ffac14dc07394b4bd
Pablo Ferreras <artgomad@gmail.com>
2019-04-08 09:09:49 +0200

    a

--- wheelchair3/Algorythim to detect posture/4_predict_&_Actuate_DATA.py
+++ wheelchair3/Algorythim to detect posture/4_predict_&_Actuate_DATA.py
@@ -179,12 +179,12 @@ def serial_to_property_values():
                 print("Start the Yoga session")
                 prev_button_value = button_value
 
-                # Writes the button value in the BUTTON GATT CHARACTERISTIC
-                my_device.char_write(GATT_CHARACTERISTIC_BUTTON, bytes(button_value))
-
                 play_sound('/home/pi/wheelchair-design-platform/docs/workshops/audios/1_intro_yoga.wav', 47)
                 play_sound('/home/pi/wheelchair-design-platform/docs/workshops/audios/2_intro_postures.wav', 11)
                 sendByBluetooth([1])
+
+                # Writes the button value in the BUTTON GATT CHARACTERISTIC
+                my_device.char_write(GATT_CHARACTERISTIC_BUTTON, bytes(button_value))
 
 
         except:
